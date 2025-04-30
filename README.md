efbf223f719fd635994bf12519812d593a1e44c8
Prashanth Babu T <prashanthbabu07@gmail.com>
2025-04-30 21:31:26 +0530

    added readme and formatted code

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # nvim-customization
- 
+
+ripgrep is a very fast and efficient command-line tool for searching directories recursively for a regex pattern. Telescope uses it for its live_grep functionality, which allows you to search for text within your project files.
+```
+brew install ripgrep
+```
+
+
+fd is a simple, fast, and user-friendly alternative to the traditional find command. Telescope can use it for faster and more convenient file finding
+```
+brew install fd
+```
+
+Install the Lua formatter:
+```
+brew install stylua
+```
--- init.lua
+++ init.lua
@@ -12,17 +12,17 @@ vim.g.mapleader = " "
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -34,46 +34,68 @@ vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
-    -- add your plugins here
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-    {
-        'nvim-telescope/telescope.nvim', tag = '0.1.8',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-            "MunifTanjim/nui.nvim",
-        },
-        lazy = false, -- neo-tree will lazily load itself
-        opts = {},
-    }
-  },
+	spec = {
+		-- add your plugins here
+		{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+		{
+			"nvim-telescope/telescope.nvim",
+			tag = "0.1.8",
+			dependencies = { "nvim-lua/plenary.nvim" },
+		},
+		{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+		{
+			"nvim-neo-tree/neo-tree.nvim",
+			branch = "v3.x",
+			dependencies = {
+				"nvim-lua/plenary.nvim",
+				"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+				"MunifTanjim/nui.nvim",
+			},
+			lazy = false, -- neo-tree will lazily load itself
+			opts = {},
+		},
+		{
+			"neovim/nvim-lspconfig",
+			config = function()
+				require("lspconfig").lua_ls.setup({})
+			end,
+		},
+		{
+			"nvimtools/none-ls.nvim",
+			config = function()
+				local null_ls = require("null-ls")
+				null_ls.setup({
+					sources = {
+						null_ls.builtins.formatting.stylua,
+					},
+				})
+			end,
+		},
+	},
 
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	install = { colorscheme = { "habamax" } },
+	-- automatically check for plugin updates
+	checker = { enabled = true },
 })
 
 local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 
-vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
-vim.keymap.set('n', '<C-m>', ':Neotree close<CR>', {})
+vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+vim.keymap.set("n", "<C-m>", ":Neotree close<CR>", {})
+
+vim.keymap.set("n", "<leader>f", function()
+	vim.lsp.buf.format()
+end, { desc = "Format current buffer" })
 
 local config = require("nvim-treesitter.configs")
 config.setup({
-    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-    highlight = { enable = true },
-    indent = { enable = true },
+	ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+	highlight = { enable = true },
+	indent = { enable = true },
 })
 
 require("catppuccin").setup()
