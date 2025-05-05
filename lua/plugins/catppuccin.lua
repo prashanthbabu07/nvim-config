1443eb62c9cf8c4597a38a0c948c048667710d02
Prashanth Babu T <prashanthbabu07@gmail.com>
2025-05-05 21:55:32 +0530

    added go debugger

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,18 +1,16 @@
 -- https://github.com/catppuccin/nvim
 
-
 return {
 	"catppuccin/nvim",
 	name = "catppuccin",
 	priority = 1000,
 	config = function()
-    --[[    require("catppuccin").setup({
+		--[[    require("catppuccin").setup({
             flavour = "latte"
-        }) ]] 
-        -- vim.g.catppuccin_flavour = "latte"
-        -- vim.g.catppuccin_flavour = "latte"
-        -- vim.cmd.colorscheme = "catppuccin"
-        -- vim.cmd.colorscheme "catppuccin-mocha"
-
-end,
+        }) ]]
+		-- vim.g.catppuccin_flavour = "latte"
+		-- vim.g.catppuccin_flavour = "latte"
+		-- vim.cmd.colorscheme = "catppuccin"
+		-- vim.cmd.colorscheme "catppuccin-mocha"
+	end,
 }
