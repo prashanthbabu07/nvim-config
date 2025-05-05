fe3260a90341a7c6bf523b83b60c56ce92119b46
Prashanth Babu T <prashanthbabu07@gmail.com>
2025-05-05 16:27:21 +0530

    Update lsp-config.lua

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -25,7 +25,8 @@ return {
             lspconfig.lua_ls.setup({})
             lspconfig.ts_ls.setup({})
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
+            vim.keymap.set('n', '<leader>gr', vim.lsp.buf.declaration, {})
             vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
             vim.keymap.set({'n', 'v'}, '<leader>E', vim.diagnostic.open_float, {})
         end
