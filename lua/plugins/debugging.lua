481b0836785bfe35ee5e5c66e6a07555ac657921
Prashanth Babu T <prashanthbabu07@gmail.com>
2025-05-07 13:23:56 +0530

    Update debugging.lua

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,61 +1,72 @@
 return {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-        "nvim-neotest/nvim-nio",
-        "rcarriga/nvim-dap-ui",
-        "leoluz/nvim-dap-go",
-    },
-    config = function()
-        local dap = require("dap")
-        local dapui = require("dapui")
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"nvim-neotest/nvim-nio",
+		"rcarriga/nvim-dap-ui",
+		"leoluz/nvim-dap-go",
+	},
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
 
-        require("dapui").setup()
-        require("dap-go").setup()
+		require("dapui").setup()
+		require("dap-go").setup()
 
-        dap.adapters.coreclr = {
-            type = "executable",
-            command = "/usr/local/share/dotnet/netcoredbg/netcoredbg",
-            -- command = "netcoredbg",
-            args = { "--interpreter=vscode" },
-        }
+		local debugger_command
 
-        dap.configurations.cs = {
-            {
-                type = "coreclr",
-                name = "launch - netcoredbg",
-                request = "launch",
-                program = function()
-                    return vim.fn.input("Path to dll", vim.fn.getcwd() .. "", "file")
-                end,
-            },
-        }
+		-- Check the operating system
+		if vim.fn.has("mac") == 1 then
+			debugger_command = "/usr/local/share/dotnet/netcoredbg"
+		elseif vim.fn.has("unix") == 1 then
+			debugger_command = "/opt/tools/bin/netcoredbg"
+		else
+			print("Unsupported OS")
+		end
 
-        dap.listeners.before.attach.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.launch.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.event_terminated.dapui_config = function()
-            dapui.close()
-        end
-        dap.listeners.before.event_exited.dapui_config = function()
-            dapui.close()
-        end
+		dap.adapters.coreclr = {
+			type = "executable",
+			-- command = "/usr/local/share/dotnet/netcoredbg/netcoredbg",
+			command = debugger_command,
+			args = { "--interpreter=vscode" },
+		}
 
-        vim.keymap.set("n", "<Leader>b", dap.toggle_breakpoint, {})
-        vim.keymap.set("n", "<F5>", dap.continue, {})
-        vim.keymap.set("n", "<F10>", function()
-            require("dap").step_over()
-        end)
-        vim.keymap.set("n", "<F11>", function()
-            require("dap").step_into()
-        end)
+		dap.configurations.cs = {
+			{
+				type = "coreclr",
+				name = "launch - netcoredbg",
+				request = "launch",
+				program = function()
+					return vim.fn.input("Path to dll", vim.fn.getcwd() .. "", "file")
+				end,
+			},
+		}
 
-       -- breakpoint character for visual queue
-        vim.fn.sign_define("DapBreakpoint", { text = "🛑", texthl = "red", linehl = "", numhl = "" })
-        vim.fn.sign_define("DapBreakpointCondition", { text = "🛑", texthl = "red", linehl = "", numhl = "" })
-        vim.fn.sign_define("DapBreakpointRejected", { text = "❌", texthl = "red", linehl = "", numhl = "" })
-        -- vim.fn.sign_define("DapLogPoint", { text = "💬", texthl = "", linehl = "", numhl = "" }) 
-    end,
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+
+		vim.keymap.set("n", "<Leader>b", dap.toggle_breakpoint, {})
+		vim.keymap.set("n", "<F5>", dap.continue, {})
+		vim.keymap.set("n", "<F10>", function()
+			require("dap").step_over()
+		end)
+		vim.keymap.set("n", "<F11>", function()
+			require("dap").step_into()
+		end)
+
+		-- breakpoint character for visual queue
+		vim.fn.sign_define("DapBreakpoint", { text = "🛑", texthl = "red", linehl = "", numhl = "" })
+		vim.fn.sign_define("DapBreakpointCondition", { text = "🛑", texthl = "red", linehl = "", numhl = "" })
+		vim.fn.sign_define("DapBreakpointRejected", { text = "❌", texthl = "red", linehl = "", numhl = "" })
+		-- vim.fn.sign_define("DapLogPoint", { text = "💬", texthl = "", linehl = "", numhl = "" })
+	end,
 }
