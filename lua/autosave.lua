acfcf9647020fedfaa382170ee6a902c1e0008c4
Prashanth Babu T <prashanthbabu07@gmail.com>
2025-05-08 15:01:23 +0530

    config changes

--- lua/autosave.lua
+++ lua/autosave.lua
@@ -1,12 +1,21 @@
 -- This script automatically saves the current buffer when leaving insert mode or when text changes.
 
-vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
-    pattern = "*",
-    callback = function()
-        if vim.bo.modified then
-            vim.cmd("silent! write")
-            vim.notify("Auto-saved!", vim.log.levels.INFO, { title = "Neovim" })
-        end
-    end,
-})
+local autosave_timer = nil
 
+vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+	pattern = "*",
+	callback = function()
+		if vim.bo.modified then
+			-- Cancel the previous timer if still running
+			if autosave_timer then
+				autosave_timer:stop()
+			end
+			-- Set a short delay (e.g., 500ms) before saving test 
+			autosave_timer = vim.defer_fn(function()
+				vim.cmd("silent! write")
+				vim.api.nvim_echo({ { "Auto-saved!", "None" } }, false, {})
+                -- vim.notify("Auto-saved!", vim.log.levels.INFO, { title = "Neovim" })
+			end, 500)
+		end
+	end,
+})
--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,49 +1,49 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		"MunifTanjim/nui.nvim",
-	},
-	lazy = false, -- neo-tree will lazily load itself
-	opts = {
-		default_component_configs = {
-			icon = {
-				folder_closed = "[+]",
-				folder_open = "[-]",
-				folder_empty = "[ ]",
-				default = "•",
-			},
-		},
-	},
-	config = function()
-		local neotree = require("neo-tree")
-		neotree.setup({
-			filesystem = {
-				filtered_items = {
-					hide_dotfiles = false,
-					hide_gitignored = false,
-					never_show = { ".DS_Store", "thumbs.db" },
-				},
-				follow_current_file = true,
-				use_libuv_file_watcher = true,
-			},
-		})
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+    },
+    lazy = false, -- neo-tree will lazily load itself
+    opts = {
+        default_component_configs = {
+            icon = {
+                folder_closed = "[+]",
+                folder_open = "[-]",
+                folder_empty = "[ ]",
+                default = "•",
+            },
+        },
+    },
+    config = function()
+        local neotree = require("neo-tree")
+        neotree.setup({
+            filesystem = {
+                filtered_items = {
+                    hide_dotfiles = false,
+                    hide_gitignored = false,
+                    never_show = { ".DS_Store", "thumbs.db" },
+                },
+                follow_current_file = true,
+                use_libuv_file_watcher = true,
+            },
+        })
 
-		-- vim.api.nvim_set_hl(0, 'NeoTreeLine', { bg = 'none' })
-		-- vim.keymap.set("n", "<C-b>", ":Neotree toggle reveal=true position=left<CR>", {})
-		-- normal, insert, visual, visual block
-		vim.keymap.set(
-			{ "n", "i", "v", "x" },
-			"<C-n>",
-			"<Esc>:Neotree toggle position=left<CR>",
-			{ noremap = true, silent = true }
-		)
+        -- vim.api.nvim_set_hl(0, 'NeoTreeLine', { bg = 'none' })
+        -- vim.keymap.set("n", "<C-b>", ":Neotree toggle reveal=true position=left<CR>", {})
+        -- normal, insert, visual, visual block
+        vim.keymap.set(
+            { "n", "i", "v", "x" },
+            "<C-n>",
+            "<Esc>:Neotree toggle position=left<CR>",
+            { noremap = true, silent = true }
+        )
 
-		-- terminal mode
-		vim.keymap.set("t", "<C-n>", "<C-\\><C-n>:Neotree toggle position=left<CR>", { noremap = true, silent = true })
-	end,
+        -- terminal mode
+        vim.keymap.set("t", "<C-n>", "<C-\\><C-n>:Neotree toggle position=left<CR>", { noremap = true, silent = true })
+    end,
 }
