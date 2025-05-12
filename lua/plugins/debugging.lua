da09be6e5eff6fb6e426592c9a197936a4c12ec9
Prashanth Babu T <prashanthbabu07@gmail.com>
2025-05-12 15:45:29 +0530

    Added dotnet debugging config

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,43 +1,118 @@
+local function rebuild_project(co, path)
+	local spinner = require("easy-dotnet.ui-modules.spinner").new()
+	spinner:start_spinner("Building")
+	vim.fn.jobstart(string.format("dotnet build %s", path), {
+		on_exit = function(_, return_code)
+			if return_code == 0 then
+				spinner:stop_spinner("Built successfully")
+			else
+				spinner:stop_spinner("Build failed with exit code " .. return_code, vim.log.levels.ERROR)
+				error("Build failed")
+			end
+			coroutine.resume(co)
+		end,
+	})
+	coroutine.yield()
+end
+
+local function dotnet_debugging_config(dap)
+	local dotnet = require("easy-dotnet")
+	local function file_exists(path)
+		local stat = vim.loop.fs_stat(path)
+		return stat and stat.type == "file"
+	end
+
+	local debug_dll = nil
+
+	local function ensure_dll()
+		if debug_dll ~= nil then
+			return debug_dll
+		end
+		local dll = dotnet.get_debug_dll()
+		debug_dll = dll
+		return dll
+	end
+
+	for _, value in ipairs({ "cs", "fsharp" }) do
+		dap.configurations[value] = {
+			{
+				type = "coreclr",
+				name = "Program",
+				request = "launch",
+				env = function()
+					local dll = ensure_dll()
+					local vars = dotnet.get_environment_variables(dll.project_name, dll.absolute_project_path)
+					return vars or nil
+				end,
+				program = function()
+					local dll = ensure_dll()
+					local co = coroutine.running()
+					rebuild_project(co, dll.project_path)
+					if not file_exists(dll.target_path) then
+						error("Project has not been built, path: " .. dll.target_path)
+					end
+					return dll.target_path
+				end,
+				cwd = function()
+					local dll = ensure_dll()
+					return dll.absolute_project_path
+				end,
+			},
+		}
+
+		dap.listeners.before["event_terminated"]["easy-dotnet"] = function()
+			debug_dll = nil
+		end
+
+		dap.adapters.coreclr = {
+			type = "executable",
+			command = "netcoredbg",
+			args = { "--interpreter=vscode" },
+		}
+	end
+end
+
 return {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-        "nvim-neotest/nvim-nio",
-        "rcarriga/nvim-dap-ui",
-        "leoluz/nvim-dap-go",
-    },
-    config = function()
-        local dap = require("dap")
-        local dapui = require("dapui")
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"nvim-neotest/nvim-nio",
+		"rcarriga/nvim-dap-ui",
+		"leoluz/nvim-dap-go",
+	},
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
 
-        require("dapui").setup()
-        require("dap-go").setup()
+		require("dapui").setup()
+		require("dap-go").setup()
+		dotnet_debugging_config(dap)
 
-        dap.listeners.before.attach.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.launch.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.event_terminated.dapui_config = function()
-            dapui.close()
-        end
-        dap.listeners.before.event_exited.dapui_config = function()
-            dapui.close()
-        end
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
 
-        vim.keymap.set("n", "<Leader>B", dap.toggle_breakpoint, { desc = "Toggle Breakpoint" })
-        vim.keymap.set("n", "<F5>", dap.continue, { desc = "Continue" })
-        vim.keymap.set("n", "<F10>", function()
-            require("dap").step_over()
-        end, { desc = "Step Over" })
-        vim.keymap.set("n", "<F11>", function()
-            require("dap").step_into()
-        end, { desc = "Step Into" })
+		vim.keymap.set("n", "<Leader>B", dap.toggle_breakpoint, { desc = "Toggle Breakpoint" })
+		vim.keymap.set("n", "<F5>", dap.continue, { desc = "Continue" })
+		vim.keymap.set("n", "<F10>", function()
+			require("dap").step_over()
+		end, { desc = "Step Over" })
+		vim.keymap.set("n", "<F11>", function()
+			require("dap").step_into()
+		end, { desc = "Step Into" })
 
-        -- breakpoint character for visual queue
-        vim.fn.sign_define("DapBreakpoint", { text = "🔴", texthl = "red", linehl = "", numhl = "" })
-        vim.fn.sign_define("DapBreakpointCondition", { text = "🔴", texthl = "red", linehl = "", numhl = "" })
-        vim.fn.sign_define("DapBreakpointRejected", { text = "❌", texthl = "red", linehl = "", numhl = "" })
-        -- vim.fn.sign_define("DapLogPoint", { text = "💬", texthl = "", linehl = "", numhl = "" })
-    end,
+		-- breakpoint character for visual queue
+		vim.fn.sign_define("DapBreakpoint", { text = "🔴", texthl = "red", linehl = "", numhl = "" })
+		vim.fn.sign_define("DapBreakpointCondition", { text = "🔴", texthl = "red", linehl = "", numhl = "" })
+		vim.fn.sign_define("DapBreakpointRejected", { text = "❌", texthl = "red", linehl = "", numhl = "" })
+		-- vim.fn.sign_define("DapLogPoint", { text = "💬", texthl = "", linehl = "", numhl = "" })
+	end,
 }
