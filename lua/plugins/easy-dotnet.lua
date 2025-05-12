2fb39c9697108cb9adff699ffa033424319c20cc
Prashanth Babu T <prashanthbabu07@gmail.com>
2025-05-12 10:00:27 +0530

    Update easy-dotnet.lua

--- lua/plugins/easy-dotnet.lua
+++ lua/plugins/easy-dotnet.lua
@@ -1,15 +1,16 @@
 return {
-    "GustavEikaas/easy-dotnet.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
-    config = function()
-        local dotnet = require("easy-dotnet")
-        dotnet.setup({
-            test_runner = {
-                viewmode = "float",
-                enable_buffer_test_execution = true, --Experimental, run tests directly from buffer
-                noBuild = true,
-                noRestore = true,
-            }
-        })
-    end,
+	"GustavEikaas/easy-dotnet.nvim",
+	commit = "50e3d11c16ef80df475e0c92248cdc066bc9fc0a",
+	dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+	config = function()
+		local dotnet = require("easy-dotnet")
+		dotnet.setup({
+			test_runner = {
+				viewmode = "float",
+				enable_buffer_test_execution = true, --Experimental, run tests directly from buffer
+				noBuild = true,
+				noRestore = true,
+			},
+		})
+	end,
 }
