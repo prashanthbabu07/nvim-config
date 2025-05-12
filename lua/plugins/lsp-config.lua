24df0ec7d1058a88d32277bd69b8303ddd395bbb
Prashanth Babu T <prashanthbabu07@gmail.com>
2025-05-12 09:30:01 +0530

    Update lsp-config.lua

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,105 +1,117 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        lazy = false,
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        lazy = false,
-        opts = {
-            auto_install = true,
-        },
-    },
-    {
-        "neovim/nvim-lspconfig",
-        lazy = false,
-        config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"ts_ls",
+					"rust_analyzer",
+					"csharp_ls",
+					"pyright",
+					"ruff",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            local lspconfig = require("lspconfig")
-            lspconfig.ts_ls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.html.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.csharp_ls.setup({
-                cmd = { "csharp-ls" }, -- Ensure csharp-ls is in your PATH
-                on_attach = function(client, bufnr)
-                    local opts = { buffer = bufnr }
-                    vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
-                end,
-                root_dir = lspconfig.util.root_pattern("*.sln", ".git", "*.csproj"),
-            })
+			local lspconfig = require("lspconfig")
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.csharp_ls.setup({
+				cmd = { "csharp-ls" }, -- Ensure csharp-ls is in your PATH
+				on_attach = function(client, bufnr)
+					local opts = { buffer = bufnr }
+					vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
+				end,
+				root_dir = lspconfig.util.root_pattern("*.sln", ".git", "*.csproj"),
+			})
 
-            lspconfig.rust_analyzer.setup({
-                capabilities = capabilities,
-            })
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities,
+			})
 
-            -- PYRIGHT: type-check, code nav
-            lspconfig.pyright.setup({
-                capabilities = capabilities,
-                on_attach = function(client, bufnr)
-                    -- disable pyright formatting
-                    client.server_capabilities.documentFormattingProvider = false
-                end,
-            })
+			-- PYRIGHT: type-check, code nav
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+				on_attach = function(client, bufnr)
+					-- disable pyright formatting
+					client.server_capabilities.documentFormattingProvider = false
+				end,
+			})
 
-            -- RUFF (the new LSP)
-            lspconfig.ruff.setup({
-                capabilities = capabilities,
-                on_attach = function(client, bufnr)
-                    -- optional: disable hover to let pyright handle it
-                    client.server_capabilities.hoverProvider = false
-                end,
-                init_options = {
-                    settings = {
-                        args = {}, -- you can pass custom ruff args here if needed
-                    },
-                },
-            })
+			-- RUFF (the new LSP)
+			lspconfig.ruff.setup({
+				capabilities = capabilities,
+				on_attach = function(client, bufnr)
+					-- optional: disable hover to let pyright handle it
+					client.server_capabilities.hoverProvider = false
+				end,
+				init_options = {
+					settings = {
+						args = {}, -- you can pass custom ruff args here if needed
+					},
+				},
+			})
 
-            vim.diagnostic.config({
-                virtual_text = {
-                    severity = { min = vim.diagnostic.severity.HINT },
-                },
-                signs = true,
-                underline = true,
-                update_in_insert = false,
-                severity_sort = true,
-            })
+			vim.diagnostic.config({
+				virtual_text = {
+					severity = { min = vim.diagnostic.severity.HINT },
+				},
+				signs = true,
+				underline = true,
+				update_in_insert = false,
+				severity_sort = true,
+			})
 
-            vim.keymap.set("n", "<leader>lsH", function()
-                vim.lsp.buf.hover({
-                    border = "rounded",
-                })
-            end, { desc = "Hover" })
-            vim.keymap.set("n", "<leader>lsd", vim.lsp.buf.definition, { desc = "Go to definition" })
-            vim.keymap.set("n", "<leader>lsD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
-            vim.keymap.set({ "n", "v" }, "<leader>lca", vim.lsp.buf.code_action, { desc = "Code action" })
-            vim.keymap.set({ "n", "v" }, "<leader>ld", function()
-                vim.diagnostic.open_float({
-                    border = "rounded",
-                })
-            end, {}, { desc = "Show diagnostics" })
-            vim.keymap.set("n", "<leader>lsh", function()
-                vim.lsp.buf.signature_help({
-                    border = "rounded",
-                })
-            end, { desc = "Signature help" })
+			vim.keymap.set("n", "<leader>lsH", function()
+				vim.lsp.buf.hover({
+					border = "rounded",
+				})
+			end, { desc = "Hover" })
+			vim.keymap.set("n", "<leader>lsd", vim.lsp.buf.definition, { desc = "Go to definition" })
+			vim.keymap.set("n", "<leader>lsD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+			vim.keymap.set({ "n", "v" }, "<leader>lca", vim.lsp.buf.code_action, { desc = "Code action" })
+			vim.keymap.set({ "n", "v" }, "<leader>ld", function()
+				vim.diagnostic.open_float({
+					border = "rounded",
+				})
+			end, {}, { desc = "Show diagnostics" })
+			vim.keymap.set("n", "<leader>lsh", function()
+				vim.lsp.buf.signature_help({
+					border = "rounded",
+				})
+			end, { desc = "Signature help" })
 
-            vim.keymap.set(
-                "n",
-                "<leader>lsr",
-                require("telescope.builtin").lsp_references,
-                { desc = "Find references (Telescope)" }
-            )
-        end,
-    },
+			vim.keymap.set(
+				"n",
+				"<leader>lsr",
+				require("telescope.builtin").lsp_references,
+				{ desc = "Find references (Telescope)" }
+			)
+		end,
+	},
 }
