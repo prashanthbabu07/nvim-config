42d3183f8ec6007e8c6fa4e7574cd00bdd9d5556
Prashanth Babu T <prashanthbabu07@gmail.com>
2025-05-16 22:51:18 +0530

    Update autosave.lua

--- lua/autosave.lua
+++ lua/autosave.lua
@@ -1,23 +1,33 @@
 -- This script automatically saves the current buffer when leaving insert mode or when text changes.
 
--- local autosave_timer = nil
---
--- vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
--- 	pattern = "*",
--- 	callback = function()
--- 		if vim.bo.modified then
--- 			-- Cancel the previous timer if still running
--- 			if autosave_timer then
--- 				autosave_timer:stop()
--- 			end
--- 			-- Set a short delay (e.g., 500ms) before saving test
--- 			autosave_timer = vim.defer_fn(function()
--- 				vim.cmd("silent! write")
--- 				vim.api.nvim_echo({ { "Auto-saved!", "None" } }, false, {})
--- 				-- vim.notify("Auto-saved!", vim.log.levels.INFO, { title = "Neovim" })
--- 			end, 500)
--- 		end
--- 	end,
--- })
+local autosave_timer = nil
 
-vim.api.nvim_create_autocmd({ "VimSuspend", "FocusLost" }, { command = "w" })
+local save_with_timer = function()
+    if vim.bo.modified then
+        -- Cancel the previous timer if still running
+        if autosave_timer then
+            autosave_timer:stop()
+        end
+        -- Set a short delay (e.g., 500ms) before saving test
+        autosave_timer = vim.defer_fn(function()
+            vim.cmd("silent! write")
+            vim.api.nvim_echo({ { "Auto-saved!", "None" } }, false, {})
+            -- vim.notify("Auto-saved!", vim.log.levels.INFO, { title = "Neovim" })
+        end, 500)
+    end
+end
+
+local auto_save = function()
+    if vim.bo.modifiable and vim.bo.modified then
+        vim.cmd("silent! write")
+        vim.api.nvim_echo({ { "Auto-saved!", "None" } }, false, {})
+    end
+end
+
+vim.api.nvim_create_autocmd({ "FocusLost", "VimSuspend", "BufLeave" }, {
+    callback = auto_save,
+})
+
+-- vim.api.nvim_create_autocmd({ "VimSuspend", "FocusLost" }, { command = "w" })
+--
+--
