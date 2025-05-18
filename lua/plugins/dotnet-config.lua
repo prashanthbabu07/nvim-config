d23cd41418e332d5707878056429c71d8421d4c6
Prashanth Babu T <prashanthbabu07@gmail.com>
2025-05-18 12:57:54 +0530

    Update dotnet-config.lua

--- lua/plugins/dotnet-config.lua
+++ lua/plugins/dotnet-config.lua
@@ -1,6 +1,6 @@
 return {
 	"GustavEikaas/easy-dotnet.nvim",
-	commit = "50e3d11c16ef80df475e0c92248cdc066bc9fc0a",
+	-- commit = "50e3d11c16ef80df475e0c92248cdc066bc9fc0a",
 	dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
 	config = function()
 		local dotnet = require("easy-dotnet") -- .setup()
