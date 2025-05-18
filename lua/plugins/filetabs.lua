a754d9e5ef92c51d0c72af477b8f31623ceae6c7
Prashanth Babu T <prashanthbabu07@gmail.com>
2025-05-18 11:46:17 +0530

    Update filetabs.lua

--- lua/plugins/filetabs.lua
+++ lua/plugins/filetabs.lua
@@ -1,29 +1,36 @@
-return {
-	"akinsho/bufferline.nvim",
-	dependencies = "nvim-tree/nvim-web-devicons",
-	config = function()
-		vim.opt.termguicolors = true
-		local bufferline = require("bufferline")
-		bufferline.setup({
-			options = {
-				mode = "buffers", -- Display tab pages instead of buffers
-				offsets = {
-					{
-						filetype = "neo-tree",
-						text = "File Explorer",
-						text_align = "left",
-						separator = true,
-					},
-				},
-				diagnostics = "nvim_lsp", -- Optional: show LSP diagnostics in the tabline
-				separator_style = "slant", -- Optional: choose your preferred separator style
-				show_close_icon = false, -- Optional: hide the close icon
-				show_tab_indicators = true,
-			},
-		})
+--[[
+documentation 
+    https://github.com/akinsho/bufferline.nvim/blob/main/doc/bufferline.txt
+--]]
 
-		--  Key mappings for bufferline
-		vim.keymap.set("n", "<S-n>", ":BufferLineCycleNext<CR>", {})
-		vim.keymap.set("n", "<S-p>", ":BufferLineCyclePrev<CR>", {})
-	end,
+return {
+    "akinsho/bufferline.nvim",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = function()
+        vim.opt.termguicolors = true
+        local bufferline = require("bufferline")
+        bufferline.setup({
+            options = {
+                mode = "buffers", -- Display tab pages instead of buffers
+                close_command = "bdelete! %d",
+                style_preset = bufferline.style_preset.no_italic,
+                offsets = {
+                    {
+                        filetype = "neo-tree",
+                        text = "File Explorer",
+                        text_align = "left",
+                        separator = true,
+                    },
+                },
+                diagnostics = "nvim_lsp", -- Optional: show LSP diagnostics in the tabline
+                -- separator_style = "slant", -- Optional: choose your preferred separator style
+                show_close_icon = true, -- Optional: hide the close icon
+                show_tab_indicators = true,
+            },
+        })
+
+        --  Key mappings for bufferline
+        vim.keymap.set("n", "<S-n>", ":BufferLineCycleNext<CR>", {})
+        vim.keymap.set("n", "<S-p>", ":BufferLineCyclePrev<CR>", {})
+    end,
 }
