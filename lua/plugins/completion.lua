f5002ddc1a3fbc8021fe7cd046007cdc37f3b4a6
Prashanth Babu T <prashanthbabu07@gmail.com>
2025-05-19 15:51:48 +0530

    removed vim-vsnip

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -8,6 +8,7 @@ return {
         dependencies = {
             "saadparwaiz1/cmp_luasnip",
             "rafamadriz/friendly-snippets",
+            -- "hrsh7th/vim-vsnip",
         },
     },
     {
@@ -22,7 +23,7 @@ return {
                 snippet = {
                     -- REQUIRED - you must specify a snippet engine
                     expand = function(args)
-                        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+                        -- vim.fn["vsnip#anonymous"](args.body)     -- For `vsnip` users.
                         require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
                     end,
                 },
@@ -35,7 +36,16 @@ return {
                     ["<C-f>"] = cmp.mapping.scroll_docs(4),
                     ["<C-Space>"] = cmp.mapping.complete(),
                     ["<C-e>"] = cmp.mapping.abort(),
-                    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                    -- ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                    ["<Tab>"] = cmp.mapping.select_next_item(),
+                    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+                    ["<CR>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.confirm({ select = true })
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
                 }),
                 sources = cmp.config.sources({
                     { name = "nvim_lsp" },
