c5d52cdf981dd8839be425d7c6fc86bc7694354b
Prashanth Babu T <prashanthbabu07@gmail.com>
2025-05-24 12:39:55 +0530

    added neo-tree dotnet templates

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,56 +1,61 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		"MunifTanjim/nui.nvim",
-	},
-	lazy = false, -- neo-tree will lazily load itself
-	opts = {
-		default_component_configs = {
-			icon = {
-				folder_closed = "[+]",
-				folder_open = "[-]",
-				folder_empty = "[ ]",
-				default = "•",
-			},
-		},
-	},
-	config = function()
-		local neotree = require("neo-tree")
-		neotree.setup({
-			filesystem = {
-				filtered_items = {
-					hide_dotfiles = false,
-					hide_gitignored = true,
-					never_show = { ".DS_Store", "thumbs.db" },
-				},
-				follow_current_file = {
-					enabled = true,
-				},
-				use_libuv_file_watcher = true,
-			},
-		})
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+    },
+    lazy = false, -- neo-tree will lazily load itself
+    opts = {
+        default_component_configs = {
+            icon = {
+                folder_closed = "[+]",
+                folder_open = "[-]",
+                folder_empty = "[ ]",
+                default = "•",
+            },
+        },
+    },
+    config = function()
+        local neotree = require("neo-tree")
+        neotree.setup({
+            filesystem = {
+                filtered_items = {
+                    hide_dotfiles = false,
+                    hide_gitignored = true,
+                    never_show = { ".DS_Store", "thumbs.db" },
+                },
+                window = {
+                    mappings = {
+                        -- Make the mapping anything you want
+                        ["E"] = "easy",
+                    },
+                },
+                follow_current_file = {
+                    enabled = true,
+                },
+                use_libuv_file_watcher = true,
+                commands = {
+                    ["easy"] = function(state)
+                        local node = state.tree:get_node()
+                        local path = node.type == "directory" and node.path or vim.fs.dirname(node.path)
+                        require("easy-dotnet").create_new_item(path, function()
+                            require("neo-tree.sources.manager").refresh(state.name)
+                        end)
+                    end,
+                },
+            },
+        })
 
-		-- vim.api.nvim_set_hl(0, 'NeoTreeLine', { bg = 'none' })
-		-- vim.keymap.set("n", "<C-b>", ":Neotree toggle reveal=true position=left<CR>", {})
-		-- normal, insert, visual, visual block
-		vim.keymap.set(
-			{ "n", "v", "t" },
-			"<leader>fe",
-			"<Esc>:Neotree toggle position=left<CR>",
-			{ noremap = true, silent = true }
-		)
-
-		-- terminal mode
-		-- vim.keymap.set(
-		-- 	"t",
-		-- 	"<leader>fe",
-		-- 	"<C-\\><C-n>:Neotree toggle position=left<CR>",
-		-- 	{ noremap = true, silent = true }
-		-- )
-	end,
+        -- normal, visual, visual block
+        vim.keymap.set(
+            { "n", "v", "t" },
+            "<leader>fe",
+            "<Esc>:Neotree toggle position=left<CR>",
+            { noremap = true, silent = true }
+        )
+    end,
 }
