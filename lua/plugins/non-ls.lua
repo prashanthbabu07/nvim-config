89b4b95101c7df8b3b0502c23dee894b79c01d94
Prashanth Babu T <prashanthbabu07@gmail.com>
2025-05-29 18:39:33 +0530

    added python for formatting

--- lua/plugins/non-ls.lua
+++ lua/plugins/non-ls.lua
@@ -1,28 +1,26 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
+    "nvimtools/none-ls.nvim",
+    config = function()
+        local null_ls = require("null-ls")
 
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua.with({
-					extra_args = { "--indent-type", "Spaces", "--indent-width", "4" },
-				}),
-				null_ls.builtins.completion.spell,
-				null_ls.builtins.formatting.prettier.with({
-					extra_args = { "--use-tabs=false", "--tab-width=4" },
-				}),
-				-- null_ls.builtins.diagnostics.eslint,
-				-- require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
-				null_ls.builtins.formatting.csharpier, -- requires csharpier installed
-				-- null_ls.builtins.diagnostics.csharp_ls,
-				-- null_ls.builtins.diagnostics.omnisharp, -- for linting if available
-			},
-			filetypes = { "lua", "javascript", "typescript", "html", "css", "csharp", "rust" },
-		})
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.stylua.with({
+                    extra_args = { "--indent-type", "Spaces", "--indent-width", "4" },
+                }),
+                null_ls.builtins.completion.spell,
+                null_ls.builtins.formatting.prettier.with({
+                    extra_args = { "--use-tabs=false", "--tab-width=4" },
+                }),
+                null_ls.builtins.formatting.csharpier, -- requires csharpier installed
+                null_ls.builtins.formatting.black,
+                null_ls.builtins.formatting.isort,
+            },
+            filetypes = { "lua", "javascript", "typescript", "html", "css", "csharp", "rust", "python" },
+        })
 
-		vim.keymap.set("n", "<leader>lsf", vim.lsp.buf.format, {
-			desc = "Format curent buffer",
-		})
-	end,
+        vim.keymap.set("n", "<leader>lsf", vim.lsp.buf.format, {
+            desc = "Format curent buffer",
+        })
+    end,
 }
