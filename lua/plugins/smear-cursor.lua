0973a739f7382bbe0696349384701bbd050e0161
Prashanth Babu T <prashanthbabu07@gmail.com>
2025-06-06 21:55:27 +0530

    added neoscroll and smear cursor.

---
+++ lua/plugins/smear-cursor.lua
@@ -0,0 +1,12 @@
+return {
+    "sphamba/smear-cursor.nvim",
+    opts = {},
+    config = function()
+        local cursor = require("smear_cursor")
+        cursor.setup({
+            stiffness = 0.8,
+            trailing_stiffness = 0.5,
+            distance_stop_animating = 0.5,
+        })
+    end,
+}
