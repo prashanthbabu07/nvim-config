57473b62bd03f6120e32fa9eef4a0d4ff7754040
Prashanth Babu T <prashanthbabu07@gmail.com>
2025-06-09 15:49:27 +0530

    added tokyonight theme

--- lua/plugins/color-themes.lua
+++ lua/plugins/color-themes.lua
@@ -78,7 +78,7 @@ return {
     {
         "projekt0n/github-nvim-theme",
         name = "github-theme",
-        lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
         priority = 1000, -- make sure to load this before all the other start plugins
         config = function()
             require("github-theme").setup({
@@ -122,5 +122,11 @@ return {
         config = function()
             require("vscode").setup({})
         end,
+    },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
     },
 }
