d1ee276620467f9e722f8a4e7d48fbbcd9afceea
Prashanth Babu T <prashanthbabu07@gmail.com>
2025-06-24 10:28:50 +0530

    removed smear-cursor
    using kitty for development.

--- lua/plugins/smear-cursor.lua
+++ lua/plugins/smear-cursor.lua
@@ -1,16 +1,18 @@
-if vim.fn.has("mac") == 1 then
-    return {}
-end
+-- if vim.fn.has("mac") == 1 then
+--     return {}
+-- end
 
-return {
-    "sphamba/smear-cursor.nvim",
-    opts = {},
-    config = function()
-        local cursor = require("smear_cursor")
-        cursor.setup({
-            stiffness = 0.9,
-            trailing_stiffness = 0.6,
-            distance_stop_animating = 0.6,
-        })
-    end,
-}
+return {}
+
+-- return {
+--     "sphamba/smear-cursor.nvim",
+--     opts = {},
+--     config = function()
+--         local cursor = require("smear_cursor")
+--         cursor.setup({
+--             stiffness = 0.9,
+--             trailing_stiffness = 0.6,
+--             distance_stop_animating = 0.6,
+--         })
+--     end,
+-- }
