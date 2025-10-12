0a4ef1f614ddc8facc78bb92f9905cef4ad58bf4
Prashanth Babu T <prashanthbabu07@gmail.com>
2025-10-12 13:07:38 +0530

    Add inlay hints roslyn

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -420,13 +420,20 @@ return {
                     ["csharp|inlay_hints"] = {
                         csharp_enable_inlay_hints_for_implicit_object_creation = true,
                         csharp_enable_inlay_hints_for_implicit_variable_types = true,
+                        csharp_enable_inlay_hints_for_types = true,
+                        dotnet_enable_inlay_hints_for_parameters = true,
                     },
                     ["csharp|code_lens"] = {
                         dotnet_enable_references_code_lens = true,
                     },
+                    ["csharp|background_analysis"] = {
+                        dotnet_analyzer_diagnostics_scope = "fullSolution",
+                        dotnet_compiler_diagnostics_scope = "fullSolution",
+                    },
                 },
             })
             vim.lsp.enable({ "roslyn" })
+            -- vim.lsp.inlay_hint.enable()
         end,
     },
 }
