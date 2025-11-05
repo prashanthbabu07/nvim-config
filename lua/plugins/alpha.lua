8b26929dfa9a8ba4079c4880ba6988e9bf4693f3
Prashanth Babu T <prashanthbabu07@gmail.com>
2025-11-05 18:07:33 +0530

    add footers

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -6,7 +6,7 @@ return {
     -- event = "VimEnter",
     config = function()
         local alpha = require("alpha")
-        local dashboard = require("alpha.themes.startify")
+        local dashboard = require("alpha.themes.dashboard")
 
         dashboard.section.header.val = {
             [[                                                                       ]],
@@ -26,12 +26,30 @@ return {
             [[                                                                       ]],
         }
 
-        -- dashboard.section.footer.val = {
-        --     "",
-        --     "",
-        --     "All these bytes will be lost in time,",
-        --     "              like tears in the rain.",
-        -- }
+        dashboard.section.footer.val = {
+            "",
+            "Yathā antaḥ tathā bāhyam",
+            "",
+            "Asato mā sad-gamaya, tamaso mā jyotir-gamaya, mrtyor mā amrutam gamaya",
+            "",
+            "Knowledge is superior to any actiona/practice. Action without understanding is inferior to knowledge.",
+        }
+        dashboard.section.buttons.val = {
+            dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+            dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+            dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+            dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+            -- dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.vim<CR>"),
+            dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+        }
+        -- dashboard.section.footer.opts.hl = "Type"
+        -- dashboard.section.header.opts.hl = "Include"
+        -- dashboard.section.buttons.opts.hl = "Keyword"
+
+        -- dashboard.opts.opts.noautocmd = true
+        -- dashboard.section.footer.opts.hl = "GruvboxGreen"
+        -- dashboard.section.header.opts.hl = "GruvboxBlue"
+        -- dashboard.section.buttons.opts.hl = "GruvboxGreen"
         alpha.setup(dashboard.opts)
     end,
 }
