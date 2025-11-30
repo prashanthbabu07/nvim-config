31a0a6ec6f725badce0eeb3e40f61567bef5a270
Prashanth Babu T <prashanthbabu07@gmail.com>
2025-11-30 20:11:31 +0530

    Comment alpha old file list

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -38,7 +38,7 @@ return {
         dashboard.section.buttons.val = {
             dashboard.button("f", "[Find file]", ":Telescope find_files cwd=./<CR>"),
             dashboard.button("e", "[New file]", ":ene <BAR> startinsert <CR>"),
-            dashboard.button("r", "[Recently used files]", ":Telescope oldfiles cwd=./<CR>"),
+            -- dashboard.button("r", "[Recently used files]", ":Telescope oldfiles cwd=./<CR>"),
             dashboard.button("t", "[Find text]", ":Telescope live_grep <CR>"),
             -- dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.vim<CR>"),
             dashboard.button("q", "[Quit Neovim]", ":qa<CR>"),
