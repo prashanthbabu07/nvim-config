f6d8c7684ff86b6eac551295a086f29e53cbea28
Prashanth Babu T <prashanthbabu07@gmail.com>
2026-05-11 20:31:57 +0530

    Switch lsp-lens to lensline and add .NET settings
    Add lsp/easy_dotnet.lua to provide C# LSP settings (enable inlay hints for implicit object creation/variable types, disable references code lens, enable organize imports on format). Replace VidocqH/lsp-lens.nvim with oribarilan/lensline.nvim in lua/plugins/lsp-lens.lua, add nvim-web-devicons dependency, and configure an inline profile with a " // " prefix and a references provider that only shows when count > 0.

--- lua/plugins/lsp-lens.lua
+++ lua/plugins/lsp-lens.lua
@@ -1,7 +1,35 @@
+-- return {
+--     "VidocqH/lsp-lens.nvim",
+--     event = "LspAttach",
+--     config = function()
+--         require("lsp-lens").setup()
+--     end,
+-- }
 return {
-    "VidocqH/lsp-lens.nvim",
+    "oribarilan/lensline.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     event = "LspAttach",
     config = function()
-        require("lsp-lens").setup()
+        require("lensline").setup({
+            profiles = {
+                {
+                    name = "default",
+                    style = {
+                        placement = "inline", -- This keeps it on the same line!
+                        prefix = " // ",      -- Subtle separator
+                    },
+                    providers = {
+                        {
+                            name = "references",
+                            enabled = true,
+                            -- Optional: only show if there are more than 0 references
+                            filter = function(count)
+                                return count > 0
+                            end,
+                        },
+                    },
+                },
+            },
+        })
     end,
 }
