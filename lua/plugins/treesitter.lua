9e364420da1c7d8971e8246df988f65ac912ba6a
Prashanth Babu T <prashanthbabu07@gmail.com>
2026-05-11 18:48:54 +0530

    add c and python to tree-sitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -18,6 +18,8 @@ return {
                 "tsx",   -- For TSX files (TypeScript React)
                 -- "jsx",   -- For JSX files (JavaScript React)
                 "json",
+                "c",
+                "python",
             }, -- list of parsers to install at the start of a neovim session
             -- border = nil, -- border style for the window (e.g. "rounded", "single"), if nil, use the default border style defined by 'vim.o.winborder'. See :h 'winborder' for more info.
             -- auto_install = false, -- if enabled, install missing parsers when editing a new file
