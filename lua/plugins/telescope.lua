83de8c137e3c68ee0be6edff7fccfaf0e8063cb3
Prashanth Babu T <prashanthbabu07@gmail.com>
2026-05-14 15:01:43 +0530

    Add telescope fzf-native extension support
    Add nvim-telescope/telescope-fzf-native.nvim as a dependency (built with make) and configure the fzf extension in telescope.setup to enable fuzzy matching, override sorters, and use smart_case. Also call require('telescope').load_extension('fzf') after setup so the extension is loaded.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,12 +5,21 @@ return {
         "nvim-telescope/telescope.nvim",
         branch = "master",
         -- tag = "0.1.8",
-        dependencies = { "nvim-lua/plenary.nvim" },
+        dependencies = { "nvim-lua/plenary.nvim", { "nvim-telescope/telescope-fzf-native.nvim", build = "make" } },
         config = function()
             local builtin = require("telescope.builtin")
             local telescope = require("telescope")
 
             telescope.setup({
+                extensions = {
+                    fzf = {
+                        fuzzy = true, -- false will only do exact matching
+                        override_generic_sorter = true, -- override the generic sorter
+                        override_file_sorter = true, -- override the file sorter
+                        case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+                        -- the default case_mode is "smart_case"
+                    },
+                },
                 defaults = {
                     -- cwd_only = true,
                     -- Use fd as the finder with basic flags
@@ -22,6 +31,10 @@ return {
                     file_ignore_patterns = { "node_modules", ".git/", "dist/", "build/" },
                 },
             })
+
+            -- To get fzf loaded and working with telescope, you need to call
+            -- load_extension, somewhere after setup function:
+            require("telescope").load_extension("fzf")
 
             vim.keymap.set("n", "<leader>ff", function()
                 builtin.find_files({
