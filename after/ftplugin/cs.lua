39f44ecf68b74b5d110e9121709a7378fad2cdf1
Prashanth Babu T <prashanthbabu07@gmail.com>
2026-05-23 19:34:54 +0530

    add marker to fold regions

--- after/ftplugin/cs.lua
+++ after/ftplugin/cs.lua
@@ -1,2 +1,15 @@
 vim.opt.foldmethod = 'marker'
 vim.opt.foldmarker = '#region,#endregion'
+
+--fold marker by default 
+vim.opt.foldenable = true
+
+-- Replace your old marker configs with these local window options
+-- vim.opt_local.foldmethod = "expr"
+-- vim.opt_local.foldexpr = "v:lua.vim.lsp.foldexpr()"
+
+-- Prevent Neovim from opening files with everything completely collapsed
+-- vim.opt_local.foldlevel = 99
+-- vim.opt_local.foldlevelstart = 99
+-- vim.opt_local.foldenable = true
+
