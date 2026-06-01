7e53c9d431ef98b51ac8676fe2d59711ad467c03
Prashanth Babu T <prashanthbabu07@gmail.com>
2026-06-01 11:46:04 +0530

    fix keymap

--- lua/vim-config.lua
+++ lua/vim-config.lua
@@ -116,5 +116,5 @@ vim.keymap.set("n", "<leader>sx", ":close<CR>", { desc = "Close split" })
 -- Navigate between splits using Ctrl + h/j/k/l
 vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left split" })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to below split" })
-vim.keymap.set("n", "<C-k", "<C-w>k", { desc = "Move to above split" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to above split" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right split" })
